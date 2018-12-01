e8ed3e0ae03350a095cbf45fc4f1dab2f5a61fb9
KierstenS12 <43118302+KierstenS12@users.noreply.github.com>
2018-12-01 09:41:47 -0500

    Update 6

--- Assets/KiSi_Scripts/KiSi_PlayerController.cs
+++ Assets/KiSi_Scripts/KiSi_PlayerController.cs
@@ -5,19 +5,21 @@ using UnityEngine.UI;
 
 public class KiSi_PlayerController : MonoBehaviour
 {
-    
+
     private Rigidbody2D rb2d;
     private bool facingRight = true;
 
     public float speed;
     public float jumpforce;
     public Text announceText;
+    public Text endText;
+    public Text lostText;
+    private int count;
+    private float timer;
 
     //audio stuff
     private AudioSource source;
     public AudioClip jumpClip;
-    private float volLowRange = .5f;
-    private float volHighRange = 1.0f;
 
     //ground check
     private bool isOnGround;
@@ -27,24 +29,37 @@ public class KiSi_PlayerController : MonoBehaviour
 
     void Start()
     {
+        count = 0;
+        timer = 0;
         rb2d = GetComponent<Rigidbody2D>();
-        announceText.text = "Catch As Many Tennis Balls As You Can!";
+        announceText.text = "Quickly, Catch Five Tennis Balls!";
+        endText.text = "";
+        SetCountText();
     }
-    
+
     void Awake()
     {
-    source = GetComponent<AudioSource>();
+        source = GetComponent<AudioSource>();
     }
-   
+
 
     void Update()
     {
-      
+
 
     }
 
     void FixedUpdate()
     {
+        //This does a timer before ending the game after 10 seconds.
+        timer = timer + Time.deltaTime;
+        if (timer >= 10)
+        {
+            endText.text = "You Lose! :(";
+            StartCoroutine(ByeAfterDelay(2));
+
+        }
+
         float moveHorizontal = Input.GetAxis("Horizontal");
         rb2d.velocity = new Vector2(moveHorizontal * speed, rb2d.velocity.y);
         isOnGround = Physics2D.OverlapCircle(groundcheck.position, checkRadius, allGround);
@@ -59,10 +74,8 @@ public class KiSi_PlayerController : MonoBehaviour
         {
             Flip();
         }
-       
-    }
 
-   
+    }
 
     void Flip()
     {
@@ -80,20 +93,39 @@ public class KiSi_PlayerController : MonoBehaviour
             if (Input.GetKey(KeyCode.UpArrow))
             {
                 rb2d.velocity = Vector2.up * jumpforce;
-                float vol = Random.Range(volLowRange, volHighRange);
                 source.PlayOneShot(jumpClip);
             }
-       
+
 
         }
     }
     void OnTriggerEnter2D(Collider2D other)
     {
-      if (other.gameObject.CompareTag("KiSi_Ball"))
+        if (other.gameObject.CompareTag("KiSi_Ball"))
         {
             other.gameObject.SetActive(false);
+            count = count + 2;
+            SetCountText();
+           // GameLoader.AddScore(1);
         }
     }
-   
+    void SetCountText()
+    {
+        //Check if we've collected all 10 pickups. If we have...
+        if (count >= 10)
+        {
+            endText.text = "You win!";
+            StartCoroutine(ByeAfterDelay(2));
+        }
+      
 
+
+    }
+    IEnumerator ByeAfterDelay(float time)
+    {
+        yield return new WaitForSeconds(time);
+
+        // Code to execute after the delay
+       // GameLoader.gameOn = false;
+    }
 }
